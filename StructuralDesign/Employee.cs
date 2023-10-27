57d099c2f8ba76700fd2061c9837adfa6c392126
narramuni <nmspavan@gmail.com>
2023-10-27 14:20:30 +0530

    Add project files.

---
+++ StructuralDesign/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructuralDesign
+{
+    internal class Employee
+    {
+        public int Empid { get; set; }
+        public string EmpName { get; set; }
+        public int Salary { get; set; }
+        public string Job { get; set; }
+
+        public Employee(int id, string name, string job, int sal)
+        {
+            Empid = id;
+            EmpName = name;
+            Salary = sal;
+            Job = job;
+        }
+
+        public class SalaryCalculations
+        {
+            public void ProcessSalary(List<Employee> employees)
+            {
+                //calculate
+                //deposit
+
+            }
+
+        }
+
+    }
+}
